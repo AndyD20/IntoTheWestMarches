fbba7dc0c86225cf1a42f5724b3fc34c40d58cb3
Andy <adelaney2011@hotmail.co.uk>
2025-10-12 02:34:55 +0100

    Move to using Threlte library for map rendering.

--- client/src/routes/map/+page.svelte
+++ client/src/routes/map/+page.svelte
@@ -1,38 +1,12 @@
 <script lang="ts">
+    import {Canvas} from "@threlte/core";
     import Map from '$lib/components/Map/map.svelte';
-    import MapImage from '$lib/assets/The_Northern_Empire.png'
-
-    // Type for the Map component instance
-    interface MapComponent {
-        resetView(): void;
-
-        fitToContainer(): void;
-    }
-
-    let mapComponent: MapComponent;
-
-    // Example with typed props
-    const mapProps = {
-        src: MapImage,
-        height: '800px',
-        minZoom: 0.8,
-        maxZoom: 3,
-        initialZoom: 1
-    };
-
-    function handleResetClick(): void {
-        mapComponent.resetView();
-    }
-
-    function handleFitClick(): void {
-        mapComponent.fitToContainer();
-    }
 </script>
 
-<Map
-  {...mapProps}
-  bind:this={mapComponent}
-/>
+<div>
+    This is the application
+</div>
 
-<button on:click={handleResetClick}>Reset</button>
-<button on:click={handleFitClick}>Fit to Screen</button>
+<Canvas>
+    <Map/>
+</Canvas>

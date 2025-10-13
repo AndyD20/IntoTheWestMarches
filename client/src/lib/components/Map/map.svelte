572e79304a1445c57721c4adf8ebcb344c962b9d
Andy <adelaney2011@hotmail.co.uk>
2025-10-13 17:59:46 +0100

    Smooth out the panning drag, change the cursor to a grab when dragging, start implementing map markers.

--- client/src/lib/components/Map/map.svelte
+++ client/src/lib/components/Map/map.svelte
@@ -1,53 +1,96 @@
 <script lang="ts">
-    import {T} from '@threlte/core';
-    import {useTexture, interactivity, useCursor} from '@threlte/extras';
-    import WorldMap from '$lib/assets/The_Northern_Empire.png';
+	import { T, useThrelte } from '@threlte/core';
+	import { useTexture, interactivity, SVG } from '@threlte/extras';
+	import WorldMap from '$lib/assets/The_Northern_Empire.png';
+	import MapPinIcon from '$lib/assets/map-pin.svg';
 
-    interactivity();
+	interactivity();
 
-    let distance = $state(5);
-    let dragging = $state(false);
-    let posX = $state(0);
-    let posY = $state(0);
+	let distance = $state(5);
+	let dragging = $state(false);
+	let posX = $state(0);
+	let posY = $state(0);
 
-    let cameraX = $state(0);
-    let cameraY = $state(0);
+	let cameraX = $state(0);
+	let cameraY = $state(0);
 
-    const zoom = (e) => {
-        distance += e.nativeEvent.deltaY / 1000;
-    };
+	let showMarker = $state(false);
+	let markerPosX = $state(0);
+	let markerPosY = $state(0);
 
-    const handleDrag = (e) => {
-        if (dragging) {
-            cameraX += (posX - e.nativeEvent.clientX) / 1000;
-            cameraY -= (posY - e.nativeEvent.clientY) / 1000;
-        }
-    };
+	const { renderer } = useThrelte();
+
+	$effect(() => {
+		if (renderer?.domElement) {
+			renderer.domElement.style.cursor = dragging ? 'grabbing' : 'pointer';
+		}
+	});
+
+	const zoom = (e) => {
+		distance += e.nativeEvent.deltaY / 1000;
+	};
+
+	const handleDrag = (e) => {
+		if (dragging) {
+			cameraX += (posX - e.nativeEvent.clientX) / 500;
+			cameraY -= (posY - e.nativeEvent.clientY) / 500;
+
+			posX = e.nativeEvent.clientX;
+			posY = e.nativeEvent.clientY;
+		}
+	};
+
+	const endDrag = () => {
+		dragging = false;
+	};
+
+	const handleOnDblClick = (e) => {
+		markerPosX = e.point.x;
+		markerPosY = e.point.y;
+		showMarker = true;
+	};
 </script>
 
 <T.PerspectiveCamera
-    makeDefault
-    position={[cameraX, cameraY, distance]}
-    fov={50}
-    oncreate={(ref) => {
-    ref.lookAt(0, 0, 0)
-  }}
+	makeDefault
+	position={[cameraX, cameraY, distance]}
+	fov={50}
+	oncreate={(ref) => {
+		ref.lookAt(0, 0, 0);
+	}}
 />
 
 {#await useTexture(WorldMap) then texture}
-    <T.Mesh
-        onwheel={(e) => zoom(e)}
-        onpointerdown={(e) => {
-            dragging = true
-            posX = e.nativeEvent.clientX
-            posY = e.nativeEvent.clientY
-        }}
-        onpointerup={(e) => {dragging = false}}
-        onpointerleave={(e) => {dragging = false}}
-        onpointerout={(e) => {dragging = false}}
-        onpointermove={(e) => handleDrag(e)}
-    >
-        <T.BoxGeometry args={[7, 4, 0]}/>
-        <T.MeshBasicMaterial map={texture}/>
-    </T.Mesh>
+	<T.Mesh
+		onwheel={(e) => zoom(e)}
+		onpointerdown={(e) => {
+			dragging = true;
+			posX = e.nativeEvent.clientX;
+			posY = e.nativeEvent.clientY;
+		}}
+		onpointerup={endDrag}
+		onpointerleave={endDrag}
+		onpointerout={endDrag}
+		onpointermove={(e) => handleDrag(e)}
+		ondblclick={handleOnDblClick}
+	>
+		<T.BoxGeometry args={[7, 4, 0]} />
+		<T.MeshBasicMaterial map={texture} />
+	</T.Mesh>
 {/await}
+
+{#if showMarker}
+    <T.Mesh
+        onpointerover={(e) => console.log(e)}
+    >
+        <SVG
+            src={MapPinIcon}
+            scale={0.005}
+            position.x={markerPosX - 0.06}
+            position.y={markerPosY + 0.12}
+            position.z={0.1}
+	    />
+    </T.Mesh>
+
+{/if}
+
